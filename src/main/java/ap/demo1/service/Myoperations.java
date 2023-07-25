b8103fbaaf611656823b26d01dbc90abfd30a80c
Gitea <gitea@fake.local>
2023-07-25 07:33:45 +0000

    demo1

---
+++ src/main/java/ap/demo1/service/Myoperations.java
@@ -0,0 +1,13 @@
+package ap.demo1.service;
+
+public class Myoperations {
+	
+	public long add(int x,int y)
+	{
+		return x+y;
+	}
+    public String getFullName(String firstname, String lastname)
+    {
+    	return firstname+' '+lastname;
+    }
+}
---
+++ src/test/java/ap/demo1/service/MyoperationsTest.java
@@ -0,0 +1,23 @@
+package ap.demo1.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyoperationsTest {
+
+	@Test
+	public void testAddPositivevalue() {
+		assertEquals(8,new Myoperations().add(3, 5));
+	}
+	@Test
+	public void testAddNegativevalue() {
+		assertEquals(-27,new Myoperations().add(-23, -4));
+	}
+
+	@Test
+	public void testGetFullName() {
+		assertEquals("Abinaya kannan",new Myoperations().getFullName("Abinaya","kannan"));
+	}
+
+}
