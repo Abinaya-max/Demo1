7ed26bb5d19a4e593f2991955a427815957c7f11
Gitea <gitea@fake.local>
2023-07-26 12:26:51 +0000

    ddd

--- src/main/java/ap/demo1/service/ddd.java
+++ src/main/java/ap/demo1/service/ddd.java
@@ -3,7 +3,6 @@ package ap.demo1.service;
 public class ddd {
 	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World");
     }
-
 }
